6b2c111719f1d36b853244a92d3aaf9dc65a4a83
Wuchieh <q20001116@gmail.com>
2025-02-20 15:09:34 +0800

    feat: add ReturnData

---
+++ return_date.go
@@ -0,0 +1,288 @@
+package ebpay
+
+// ResultCredit 信用卡支付回傳參數
+//
+//	一次付清、分期、紅利、DCC、Apple Pay、Google Pay、Samaung Pay、國民旅遊卡、銀聯、AE
+type ResultCredit struct {
+	// AuthBank 收單金融機構
+	AuthBank string `json:"AuthBank"`
+
+	// CardBank 發卡金融機構
+	CardBank string `json:"CardBank"`
+
+	// RespondCode 金融機構回應碼
+	RespondCode string `json:"RespondCode"`
+
+	// Auth 授權碼
+	Auth string `json:"Auth"`
+
+	// Card6No 卡號前六碼
+	Card6No int `json:"Card6No"`
+
+	// Card4No 卡號末四碼
+	Card4No int `json:"Card4No"`
+
+	// Inst 分期-期別 信用卡分期交易期別。
+	Inst int `json:"Inst"`
+
+	// InstFirst 分期-首期金額 信用卡分期交易首期金額。
+	InstFirst int `json:"InstFirst"`
+
+	// InstEach 分期-每期金額 信用卡分期交易每期金額。
+	InstEach int `json:"InstEach"`
+
+	// ECI
+	ECI string `json:"ECI"`
+
+	// TokenUseStatus 信用卡快速結帳使用狀態
+	TokenUseStatus int
+
+	// RedAmt 紅利折抵後實際金額
+	RedAmt int `json:"RedAmt"`
+
+	// PaymentMethod 交易類別
+	PaymentMethod string `json:"PaymentMethod"`
+
+	// DCCAmt 外幣金額
+	DCCAmt float64 `json:"DCC_Amt"`
+
+	// DCCRate 匯率
+	DCCRate float64 `json:"DCC_Rate"`
+
+	// DCCMarkup 風險匯率
+	DCCMarkup float64 `json:"DCC_Markup"`
+
+	// DCCCurrency 幣別
+	DCCCurrency string `json:"DCC_Currency"`
+
+	// DCCCurrencyCode 幣別代碼
+	DCCCurrencyCode int `json:"DCC_Currency_Code"`
+}
+
+// ATMResult WEBATM、ATM繳費回傳參數
+type ATMResult struct {
+	// PayBankCode 付款人金融機構代碼
+	PayBankCode string `json:"PayBankCode"`
+
+	// PayerAccount5Code 付款人金融機構帳號末五碼
+	PayerAccount5Code string `json:"PayerAccount5Code"`
+}
+
+// ConvenienceStoreResult 超商代碼繳費回傳參數
+type ConvenienceStoreResult struct {
+	// CodeNo 繳費代碼 String(30) 繳費代碼。
+	CodeNo string `json:"CodeNo"`
+
+	// StoreType 繳費門市類別 Int(1)
+	StoreType int `json:"StoreType"`
+
+	// StoreID 繳費門市代號 String(10) 繳費門市代號 (全家回傳門市中文名稱)
+	StoreID string `json:"StoreID"`
+}
+
+// ConvenienceStoreBarcodeResult 超商條碼繳費回傳參數
+type ConvenienceStoreBarcodeResult struct {
+	// Barcode1 第一段條碼 繳費條碼第一段條碼資料。
+	Barcode1 string `json:"Barcode_1"`
+
+	// Barcode2 第二段條碼 繳費條碼第二段條碼資料。
+	Barcode2 string `json:"Barcode_2"`
+
+	// Barcode3 第三段條碼 繳費條碼第三段條碼資料。
+	Barcode3 string `json:"Barcode_3"`
+
+	// RepayTimes 付款次數 條碼繳費付款次數
+	RepayTimes int `json:"RepayTimes"`
+
+	// PayStore 繳費超商 付款人至超商繳費，該收款超商的代碼，
+	PayStore string `json:"PayStore"`
+}
+
+// CrossBorder 超商條碼繳費回傳參數
+type CrossBorder struct {
+	// ChannelID 跨境通路類型
+	ChannelID string `json:"ChannelID"`
+
+	// ChannelNo 跨境通路 交易序號
+	ChannelNo string `json:"ChannelNo"`
+}
+
+// Logistics 超商物流回傳參數(尚未啟用)
+type Logistics struct {
+	// StoreCode 超商門市編號
+	StoreCode string
+	// StoreName 超商門市名稱
+	StoreName string
+	// StoreType 超商類別名稱
+	StoreType string
+	// StoreAddr 超商門市地址
+	StoreAddr string
+	// TradeType 取件交易方式
+	TradeType int
+	// CVSCOMName 取貨人
+	CVSCOMName string
+	// CVSCOMPhone 取貨人手機號碼
+	CVSCOMPhone string
+	// LgsNo 物流寄件單號
+	LgsNo string
+	// LgsType 物流型態
+	LgsType string
+}
+
+type Result struct {
+	// MerchantID 商店代號
+	MerchantID string `json:"MerchantID"`
+
+	// Amt 交易金額
+	Amt int `json:"Amt"`
+
+	// TradeNo 藍新金流交易序號
+	TradeNo string `json:"TradeNo"`
+
+	// MerchantOrderNo 商店訂單編號 商店自訂訂單編號
+	MerchantOrderNo string `json:"MerchantOrderNo"`
+
+	// PaymentType
+	PaymentType string `json:"PaymentType"`
+
+	// RespondType
+	RespondType string `json:"RespondType"`
+
+	// PayTime 支付完成時間
+	PayTime string `json:"PayTime"`
+
+	// IP 付款人取號或交易時的IP
+	IP string `json:"IP"`
+
+	// EscrowBank 款項保管銀行
+	EscrowBank string `json:"EscrowBank"`
+
+	// Exp
+	Exp string `json:"Exp"`
+
+	/*信用卡支付回傳參數（一次付清、分期、紅利、DCC、Apple Pay、Google Pay、Samaung Pay、國民旅遊卡、銀聯、AE）*/
+	// AuthBank 收單金融機構
+	AuthBank string `json:"AuthBank"`
+
+	// CardBank 發卡金融機構
+	CardBank string `json:"CardBank"`
+
+	// RespondCode 金融機構回應碼
+	RespondCode string `json:"RespondCode"`
+
+	// Auth 授權碼
+	Auth string `json:"Auth"`
+
+	// Card6No 卡號前六碼
+	Card6No int `json:"Card6No"`
+
+	// Card4No 卡號末四碼
+	Card4No int `json:"Card4No"`
+
+	// Inst 分期-期別 信用卡分期交易期別。
+	Inst int `json:"Inst"`
+
+	// InstFirst 分期-首期金額 信用卡分期交易首期金額。
+	InstFirst int `json:"InstFirst"`
+
+	// InstEach 分期-每期金額 信用卡分期交易每期金額。
+	InstEach int `json:"InstEach"`
+
+	// ECI
+	ECI string `json:"ECI"`
+
+	// TokenUseStatus 信用卡快速結帳使用狀態
+	TokenUseStatus int
+
+	// RedAmt 紅利折抵後實際金額
+	RedAmt int `json:"RedAmt"`
+
+	// PaymentMethod 交易類別
+	PaymentMethod string `json:"PaymentMethod"`
+
+	// DCCAmt 外幣金額
+	DCCAmt float64 `json:"DCC_Amt"`
+
+	// DCCRate 匯率
+	DCCRate float64 `json:"DCC_Rate"`
+
+	// DCCMarkup 風險匯率
+	DCCMarkup float64 `json:"DCC_Markup"`
+
+	// DCCCurrency 幣別
+	DCCCurrency string `json:"DCC_Currency"`
+
+	// DCCCurrencyCode 幣別代碼
+	DCCCurrencyCode int `json:"DCC_Currency_Code"`
+
+	/*WEBATM、ATM繳費回傳參數*/
+
+	// PayBankCode 付款人金融機構代碼
+	PayBankCode string `json:"PayBankCode"`
+
+	// PayerAccount5Code 付款人金融機構帳號末五碼
+	PayerAccount5Code string `json:"PayerAccount5Code"`
+
+	/*超商代碼繳費回傳參數 */
+
+	// CodeNo 繳費代碼 String(30) 繳費代碼。
+	CodeNo string `json:"CodeNo"`
+
+	// StoreType 繳費門市類別 Int(1)
+	StoreType int `json:"StoreType"`
+
+	// StoreID 繳費門市代號 String(10) 繳費門市代號 (全家回傳門市中文名稱)
+	StoreID string `json:"StoreID"`
+
+	/*超商條碼繳費回傳參數*/
+
+	// Barcode1 第一段條碼 繳費條碼第一段條碼資料。
+	Barcode1 string `json:"Barcode_1"`
+
+	// Barcode2 第二段條碼 繳費條碼第二段條碼資料。
+	Barcode2 string `json:"Barcode_2"`
+
+	// Barcode3 第三段條碼 繳費條碼第三段條碼資料。
+	Barcode3 string `json:"Barcode_3"`
+
+	// RepayTimes 付款次數 條碼繳費付款次數
+	RepayTimes int `json:"RepayTimes"`
+
+	// PayStore 繳費超商 付款人至超商繳費，該收款超商的代碼，
+	PayStore string `json:"PayStore"`
+
+	/*跨境支付回傳參數(包含簡單付電子錢包、簡單付微信支付、簡單付支付寶)*/
+
+	// ChannelID 跨境通路類型
+	ChannelID string `json:"ChannelID"`
+
+	// ChannelNo 跨境通路 交易序號
+	ChannelNo string `json:"ChannelNo"`
+
+	/*玉山Wallet回傳參數*/
+
+	// RedDisAmt 紅利折抵金額
+	RedDisAmt int `json:"RedDisAmt"`
+
+	/*BitoPay 回傳參數*/
+
+	// CryptoCurrency 加密貨幣代號
+	CryptoCurrency string `json:"CryptoCurrency"`
+
+	// CryptoAmount   加密貨幣數量
+	CryptoAmount string `json:"CryptoAmount"`
+
+	// CryptoRate     加密貨幣匯率
+	CryptoRate string `json:"CryptoRate"`
+
+	/*玉山Wallet/台灣Pay/BitoPay 都有*/
+
+	// PayAmt 實際付款金額
+	PayAmt int `json:"PayAmt"`
+}
+
+type ReturnDate struct {
+	Status  string `json:"Status"`
+	Message string `json:"Message"`
+	Result  Result `json:"Result"`
+}
--- vars.go
+++ vars.go
@@ -6,3 +6,77 @@ const (
 	DateTime = "20060102150405"
 	Version  = "2.2"
 )
+
+// 銀行代碼
+const (
+	BankEsun      = "Esun"      // 玉山銀行
+	BankTaishin   = "Taishin"   // 台新銀行
+	BankCTBC      = "CTBC"      // 中國信託銀行
+	BankNCCC      = "NCCC"      // 聯合信用卡中心
+	BankCathayBK  = "CathayBK"  // 國泰世華銀行
+	BankCitibank  = "Citibank"  // 花旗銀行
+	BankUBOT      = "UBOT"      // 聯邦銀行
+	BankSKBank    = "SKBank"    // 新光銀行
+	BankFubon     = "Fubon"     // 富邦銀行
+	BankFirstBank = "FirstBank" // 第一銀行
+	BankLINEBank  = "LINEBank"  // 連線商業銀行
+	BankSinoPac   = "SinoPac"   // 永豐銀行
+	BankBOT       = "BOT"       // 台灣銀行
+	BankHNCB      = "HNCB"      // 華南銀行
+)
+
+// 交易類別
+const (
+	PaymentMethodCREDIT     = "CREDIT"     // 台灣發卡機構核發之信用卡
+	PaymentMethodFOREIGN    = "FOREIGN"    // 國外發卡機構核發之卡
+	PaymentMethodUNIONPAY   = "UNIONPAY"   // 銀聯卡
+	PaymentMethodAPPLEPAY   = "APPLEPAY"   // ApplePay
+	PaymentMethodGOOGLEPAY  = "GOOGLEPAY"  // GooglePay
+	PaymentMethodSAMSUNGPAY = "SAMSUNGPAY" // SamsungPay
+	PaymentMethodDCC        = "DCC"        // 動態貨幣轉換
+)
+
+// 繳費門市類別
+const (
+	StoreTypeSEVEN  = 1 // 統一超商
+	StoreTypeFAMILY = 2 // 全家便利商店
+	StoreTypeOK     = 3 // OK便利商店
+	StoreTypeHILIFE = 4 // 萊爾富便利商店
+)
+
+// 繳費超商
+const (
+	PayStoreSEVEN  = "SEVEN"  // 統一超商
+	PayStoreFAMILY = "FAMILY" // 全家便利商店
+	PayStoreOK     = "OK"     // OK便利商店
+	PayStoreHILIFE = "HILIFE" // 萊爾富便利商店
+)
+
+// 跨境通路類型
+const (
+	ChannelIDALIPAY    = "ALIPAY"    // 支付寶
+	ChannelIDWECHATPAY = "WECHATPAY" // 微信支付
+	ChannelIDACCLINK   = "ACCLINK"   // 約定連結帳戶
+	ChannelIDCREDIT    = "CREDIT"    // 信用卡
+	ChannelIDCVS       = "CVS"       // 超商代碼
+	ChannelIDP2GEACC   = "P2GEACC"   // 簡單付電子帳戶轉帳
+	ChannelIDVACC      = "VACC"      // ATM轉帳
+	ChannelIDWEBATM    = "WEBATM"    // WebATM轉帳
+)
+
+// 加密貨幣代號
+const (
+	CryptoCurrencyBTC  = "BTC"  // 比特幣 BTC
+	CryptoCurrencyETH  = "ETH"  // 以太幣 ETH
+	CryptoCurrencyUSDT = "USDT" // 泰達幣 USDT
+)
+
+// 付款方式
+const (
+	PaymentTypeESUNWALLET = "ESUNWALLET" // 玉山 Wallet
+	PaymentTypeLINEPAY    = "LINEPAY"    // Line Pay
+	PaymentTypeTAIWANPAY  = "TAIWANPAY"  // 台灣 Pay
+	PaymentTypeEZPAY      = "EZPAY"      // ezPay 電子錢包
+	PaymentTypeEZPALIPAY  = "EZPALIPAY"  // Alipay 支付寶
+	PaymentTypeEZPWECHAT  = "EZPWECHAT"  // WeChat 微信
+)
